f6c392488fd3c21ee342acadbd90884641bf8b45
Aleksey Oblozhko <a.oblozhko@haulmont.net>
2025-05-16 10:40:40 +0400

    uncontrainted param, query method

--- webcontent/src/main/java/io/jmix/webcontent/service/WebContentService.java
+++ webcontent/src/main/java/io/jmix/webcontent/service/WebContentService.java
@@ -1,6 +1,8 @@
 package io.jmix.webcontent.service;
 
 import io.jmix.core.DataManager;
+import io.jmix.core.FluentLoader;
+import io.jmix.core.UnconstrainedDataManager;
 import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.webcontent.entity.WebContent;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +19,21 @@ public class WebContentService {
     @Autowired
     private CurrentAuthentication currentAuthentication;
 
-    public WebContent findBySlug(String slug) {
-        return dataManager.load(WebContent.class)
+    public FluentLoader.ByQuery<WebContent> queryBySlug(String slug) {
+        return queryBySlug(slug, false);
+    }
+    
+    public FluentLoader.ByQuery<WebContent> queryBySlug(String slug, boolean isUnconstrainted) {
+        UnconstrainedDataManager dm = isUnconstrainted ? dataManager.unconstrained() : dataManager;
+        return dm.load(WebContent.class)
                 .query("select distinct wc from WebContent wc " +
                         "where wc.slug = :slug and (wc.lang = :lang or wc.lang = :lang_default)")
                 .parameter("slug", slug)
                 .parameter("lang", currentAuthentication.getLocale().getLanguage())
-                .parameter("lang_default", Locale.ENGLISH.getLanguage())
-                .one();
+                .parameter("lang_default", Locale.ENGLISH.getLanguage());
+    }
+
+    public WebContent findBySlug(String slug) {
+        return queryBySlug(slug).one();
     }
 }
