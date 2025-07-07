4e714132e19a925665a7a65dbf1b7b303496488e
Aleksey Oblozhko <a.oblozhko@haulmont.net>
2025-07-07 09:28:36 +0400

    parent view changed to default main

--- webcontent/src/main/java/io/jmix/webcontent/view/webcontent/WebContentDetailView.java
+++ webcontent/src/main/java/io/jmix/webcontent/view/webcontent/WebContentDetailView.java
@@ -1,14 +1,14 @@
 package io.jmix.webcontent.view.webcontent;
 
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.DefaultMainViewParent;
 import io.jmix.flowui.view.EditedEntityContainer;
 import io.jmix.flowui.view.StandardDetailView;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
 import io.jmix.webcontent.entity.WebContent;
-import io.jmix.webcontent.view.main.MainView;
 
-@Route(value = "webContents/:id", layout = MainView.class)
+@Route(value = "webContents/:id", layout = DefaultMainViewParent.class)
 @ViewController("WebContent.detail")
 @ViewDescriptor("web-content-detail-view.xml")
 @EditedEntityContainer("webContentDc")
--- webcontent/src/main/java/io/jmix/webcontent/view/webcontent/WebContentListView.java
+++ webcontent/src/main/java/io/jmix/webcontent/view/webcontent/WebContentListView.java
@@ -1,7 +1,7 @@
 package io.jmix.webcontent.view.webcontent;
 
+import io.jmix.flowui.view.DefaultMainViewParent;
 import io.jmix.webcontent.entity.WebContent;
-import io.jmix.webcontent.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.view.DialogMode;
 import io.jmix.flowui.view.LookupComponent;
@@ -9,7 +9,7 @@ import io.jmix.flowui.view.StandardListView;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
 
-@Route(value = "webContents", layout = MainView.class)
+@Route(value = "webContents", layout = DefaultMainViewParent.class)
 @ViewController("WebContent.list")
 @ViewDescriptor("web-content-list-view.xml")
 @LookupComponent("webContentsDataGrid")
